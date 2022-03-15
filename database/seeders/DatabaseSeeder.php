142afcbb53460e6369a2800680c179be72d17266
JonssonJohannae <johanna_jonson@hotmail.com>
2022-03-15 16:55:10 +0100

    Added databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cuisine;
+use App\Models\Dish;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +19,89 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+
+        $cuisines = [
+            new Cuisine(['cuisine' => 'Sweden']),
+            new Cuisine(['cuisine' => 'Italy']),
+            new Cuisine(['cuisine' => 'France']),
+            new Cuisine(['cuisine' => 'India']),
+            new Cuisine(['cuisine' => 'Mexico']),
+            new Cuisine(['cuisine' => 'Spain']),
+        ];
+
+        foreach ($cuisines as $cuisine) {
+            $cuisine->save();
+        }
+
+        $dishes = [
+            new Dish([
+                'dish' => 'Vegan meatballs with mashed potatoes and lingonberries',
+                'cuisines_id' => 1,
+                'diet' => 'Vegan'
+            ]),
+            new Dish([
+                'dish' => 'potato hotchpotch',
+                'cuisines_id' => 1,
+                'diet' => 'Vegan'
+            ]),
+            new Dish([
+                'dish' => 'Margarita pizza',
+                'cuisines_id' => 2,
+                'diet' => 'Vegeterian'
+            ]),
+            new Dish([
+                'dish' => 'Cacio e pepe',
+                'cuisines_id' => 2,
+                'diet' => 'Vegeterian'
+            ]),
+            new Dish([
+                'dish' => 'Vegan coq au vin',
+                'cuisines_id' => 3,
+                'diet' => 'Vegan'
+            ]),
+            new Dish([
+                'dish' => 'Soupe à l’oignon (leek soup)',
+                'cuisines_id' => 3,
+                'diet' => 'Vegan'
+            ]),
+            new Dish([
+                'dish' => 'Chana masala',
+                'cuisines_id' => 4,
+                'diet' => 'Vegan'
+            ]),
+            new Dish([
+                'dish' => 'Palak Paneer',
+                'cuisines_id' => 4,
+                'diet' => 'Vegeterian'
+            ]),
+            new Dish([
+                'dish' => 'Tacos',
+                'cuisines_id' => 5,
+                'diet' => 'Vegeterian'
+            ]),
+            new Dish([
+                'dish' => 'Burrito',
+                'cuisines_id' => 5,
+                'diet' => 'Vegeterian'
+            ]),
+            new Dish([
+                'dish' => 'Paella',
+                'cuisines_id' => 6,
+                'diet' => 'Vegan'
+            ]),
+            new Dish([
+                'dish' => 'Tapas',
+                'cuisines_id' => 6,
+                'diet' => 'Vegeterian'
+            ]),
+
+
+        ];
+        foreach ($dishes as $dish) {
+            $dish->save();
+        }
     }
 }
+$user = new User(['name' => 'Henrik', 'email' => 'henrik@yrgo.se', 'password' => Hash::make('123')]);
+$user->save();
